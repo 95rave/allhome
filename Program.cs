414bab82565eb5b299b49bfbbc3a12e0337e7f2b
95rave <ravanaap@code.edu.az>
2022-04-18 16:21:29 +0400

    13.04

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sumtask2
+{
+    class Program
+    {
+        static int Getsum(int x )
+
+        {
+            int sum = 0;
+
+            while (x != 0)
+            {
+                sum = sum + x % 10;
+                x = x / 10;
+
+            }
+            return sum;
+        }
+        public static void Main()
+        {
+            int x = 1995;
+            Console.Write(Getsum(x));
+        }
+    }    
+
+
+          
+}
